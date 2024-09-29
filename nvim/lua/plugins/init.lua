0b43c0ca0eb4d2b16b13ac4bdfde427f54139df1
TheBoje <47856523+TheBoje@users.noreply.github.com>
2024-09-29 23:58:42 +0200

    update telescope fzf build steps

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -118,7 +118,7 @@ return {
 
   {
     "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
   },
 
   {
